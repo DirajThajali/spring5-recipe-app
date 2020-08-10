338fd92acfd772367349e0ce1c97a424f0bc91a6
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-10 13:39:03 -0700

    Initial Project Add

---
+++ src/main/java/com/tutorial/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,7 @@
+package com.tutorial.spring5recipeapp.domain;
+
+public class Notes {
+
+    private Recipe recipe;
+    private String recipeNotes;
+}
---
+++ src/main/java/com/tutorial/spring5recipeapp/domain/Recipe.java
@@ -0,0 +1,16 @@
+package com.tutorial.spring5recipeapp.domain;
+
+public class Recipe {
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String direction;
+    // todo add
+    //private Difficulty difficulty;
+    private Byte[] image;
+    private Notes notes;
+}
