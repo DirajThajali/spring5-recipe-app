c5abf3f1dcde136d7a2340a4d94f289aab876d75
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-10 16:26:56 -0700

    Created Category entity and established Many to Many relationship.

--- src/main/java/com/tutorial/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/com/tutorial/spring5recipeapp/domain/Ingredient.java
@@ -3,14 +3,13 @@ package com.tutorial.spring5recipeapp.domain;
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
 public class Ingredient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String description;
-
     private BigDecimal amount;
 
     @OneToOne(fetch = FetchType.EAGER)
--- src/main/java/com/tutorial/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/tutorial/spring5recipeapp/domain/Recipe.java
@@ -9,20 +9,16 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String description;
-
     private Integer prepTime;
-
     private Integer cookTime;
-
     private Integer servings;
-
     private String source;
-
     private String url;
-
     private String direction;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -33,8 +29,11 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
