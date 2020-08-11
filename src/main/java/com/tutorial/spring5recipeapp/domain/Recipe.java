5a531c7f0382a34618e6eac60856e7ee5488df2d
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-10 18:42:00 -0700

    Loaded recipes into the recipe repository on startup.

--- src/main/java/com/tutorial/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/tutorial/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.tutorial.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -18,7 +19,7 @@ public class Recipe {
     private String direction;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Lob
     private Byte[] image;
@@ -33,7 +34,7 @@ public class Recipe {
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -129,6 +130,14 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
 
