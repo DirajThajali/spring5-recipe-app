738c5973ad3aa2c2793525a849981c8f1f957df1
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-10 17:43:08 -0700

    Added Spring Data JPA Query Methods.

--- src/main/java/com/tutorial/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/tutorial/spring5recipeapp/controllers/IndexController.java
@@ -1,14 +1,34 @@
 package com.tutorial.spring5recipeapp.controllers;
 
+import com.tutorial.spring5recipeapp.domain.Category;
+import com.tutorial.spring5recipeapp.domain.UnitOfMeasure;
+import com.tutorial.spring5recipeapp.repositories.CategoryRepository;
+import com.tutorial.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
-        System.out.println("Diraj...");
+
+        Optional<Category> optionalCategory = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> optionalUnitOfMeasure = unitOfMeasureRepository.findByDescription("Ounce");
+
+        System.out.println("It should be 1: " + optionalCategory.get().getId());
+        System.out.println("It should be 5: " + optionalUnitOfMeasure.get().getId());
         return "index";
     }
 }
