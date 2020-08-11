8e6128f43ec437c510c9f33ef9add19199da2182
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-10 19:50:51 -0700

    Listed Recipes to the recipe page.

---
+++ src/main/java/com/tutorial/spring5recipeapp/controllers/RecipeController.java
@@ -0,0 +1,22 @@
+package com.tutorial.spring5recipeapp.controllers;
+
+import com.tutorial.spring5recipeapp.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping({"/recipe", "/recipe.html"})
+    public String listRecipes(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
+        return "recipe/list";
+    }
+}
---
+++ src/main/java/com/tutorial/spring5recipeapp/services/RecipeServiceImpl.java
@@ -0,0 +1,25 @@
+package com.tutorial.spring5recipeapp.services;
+
+import com.tutorial.spring5recipeapp.domain.Recipe;
+import com.tutorial.spring5recipeapp.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        HashSet<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
+    }
+}
