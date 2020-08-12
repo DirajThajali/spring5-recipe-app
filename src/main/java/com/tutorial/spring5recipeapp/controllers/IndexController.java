471bddded411dad055bd129d3e8026efa3dea2e7
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-12 13:18:29 -0700

    Refactored by adding project lombok.

--- src/main/java/com/tutorial/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/tutorial/spring5recipeapp/controllers/IndexController.java
@@ -4,11 +4,13 @@ import com.tutorial.spring5recipeapp.domain.Category;
 import com.tutorial.spring5recipeapp.domain.UnitOfMeasure;
 import com.tutorial.spring5recipeapp.repositories.CategoryRepository;
 import com.tutorial.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -23,6 +25,8 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        log.debug("getting index page");
+        log.debug("logger from Index Controller ######################################################");
 
         Optional<Category> optionalCategory = categoryRepository.findByDescription("American");
         Optional<UnitOfMeasure> optionalUnitOfMeasure = unitOfMeasureRepository.findByDescription("Ounce");
--- src/main/java/com/tutorial/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/tutorial/spring5recipeapp/controllers/RecipeController.java
@@ -1,10 +1,12 @@
 package com.tutorial.spring5recipeapp.controllers;
 
 import com.tutorial.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -16,6 +18,8 @@ public class RecipeController {
 
     @RequestMapping({"/recipe", "/recipe.html"})
     public String listRecipes(Model model) {
+        log.debug("getting recipe page");
+        log.debug("logger from Recipe Controller ######################################################");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "recipe/list";
     }
