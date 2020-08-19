ec678978bd83a7488d70b722f8f73867f547a1e2
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-19 11:24:44 -0700

    added 404 and 400 error handlers

--- src/main/java/com/tutorial/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/tutorial/spring5recipeapp/controllers/RecipeController.java
@@ -1,14 +1,14 @@
 package com.tutorial.spring5recipeapp.controllers;
 
 import com.tutorial.spring5recipeapp.commands.RecipeCommand;
+import com.tutorial.spring5recipeapp.exceptions.NotFoundException;
 import com.tutorial.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -56,5 +56,26 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND) //without this, the exception handler takes precedence over the not found error
+    @ExceptionHandler(NotFoundException.class) //so this will return status code 200
+    public ModelAndView handleNotFound(Exception exception) {
+        log.debug("Handling Not Found Exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception) {
+        log.debug("Handling Number Format Exception");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", exception);
+        modelAndView.setViewName("400error");
+        return modelAndView;
     }
 }
--- src/main/java/com/tutorial/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/tutorial/spring5recipeapp/services/RecipeServiceImpl.java
@@ -42,7 +42,7 @@ public class RecipeServiceImpl implements RecipeService {
         Optional<Recipe> recipeOptional = recipeRepository.findById(id);
         if (recipeOptional.isEmpty()) {
 //            throw new RuntimeException("Recipe not found");
-            throw new NotFoundException("Recipe Not Found");
+            throw new NotFoundException("Recipe Not Found for ID: " + id);
         }
         return recipeOptional.get();
     }
